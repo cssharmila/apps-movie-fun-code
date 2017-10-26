782bdb6b7bb000fc61422fd992aba36c7745fa6d
Sharmila Shanmugam <Sharmila.Shanmugam@cognizant.com>
2017-10-26 11:26:24 +0530

    work in progress

--- src/main/java/org/superbiz/moviefun/HomeController.java
+++ src/main/java/org/superbiz/moviefun/HomeController.java
@@ -1,6 +1,10 @@
 package org.superbiz.moviefun;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.superbiz.moviefun.albums.Album;
 import org.superbiz.moviefun.albums.AlbumFixtures;
@@ -18,12 +22,18 @@ public class HomeController {
     private final AlbumsBean albumsBean;
     private final MovieFixtures movieFixtures;
     private final AlbumFixtures albumFixtures;
+    private final PlatformTransactionManager moviesTransactionManager;
+    private final PlatformTransactionManager albumsTransactionManager;
 
-    public HomeController(MoviesBean moviesBean, AlbumsBean albumsBean, MovieFixtures movieFixtures, AlbumFixtures albumFixtures) {
+    public HomeController(MoviesBean moviesBean, AlbumsBean albumsBean, MovieFixtures movieFixtures, AlbumFixtures albumFixtures,
+                           PlatformTransactionManager moviesTransactionManager,
+                           PlatformTransactionManager albumsTransactionManager) {
         this.moviesBean = moviesBean;
         this.albumsBean = albumsBean;
         this.movieFixtures = movieFixtures;
         this.albumFixtures = albumFixtures;
+        this.moviesTransactionManager = moviesTransactionManager;
+        this.albumsTransactionManager = albumsTransactionManager;
     }
 
     @GetMapping("/")
@@ -33,13 +43,31 @@ public class HomeController {
 
     @GetMapping("/setup")
     public String setup(Map<String, Object> model) {
-        for (Movie movie : movieFixtures.load()) {
-            moviesBean.addMovie(movie);
+        TransactionDefinition moviesTXDef = new DefaultTransactionDefinition();
+        TransactionStatus moviesTXStatus = moviesTransactionManager.getTransaction(moviesTXDef);
+        try{
+            for (Movie movie : movieFixtures.load()) {
+                moviesBean.addMovie(movie);
+            }
+            moviesTransactionManager.commit(moviesTXStatus);
+        }catch(Exception e){
+            if(!moviesTXStatus.isCompleted()){
+                moviesTransactionManager.rollback(moviesTXStatus);
+            }
         }
 
-        for (Album album : albumFixtures.load()) {
-            albumsBean.addAlbum(album);
+
+        TransactionStatus albumsTXStatus = albumsTransactionManager.getTransaction(null);
+
+        try{
+            for (Album album : albumFixtures.load()) {
+                albumsBean.addAlbum(album);
+            }
+            albumsTransactionManager.commit(albumsTXStatus);
+        }catch(Exception e){
+            albumsTransactionManager.rollback(albumsTXStatus);
         }
+
 
         model.put("movies", moviesBean.getMovies());
         model.put("albums", albumsBean.getAlbums());
--- src/main/java/org/superbiz/moviefun/movies/MoviesBean.java
+++ src/main/java/org/superbiz/moviefun/movies/MoviesBean.java
@@ -29,14 +29,14 @@ import java.util.List;
 @Repository
 public class MoviesBean {
 
-    @PersistenceContext
+    @PersistenceContext(unitName = "movies-entity-factory")
     private EntityManager entityManager;
 
     public Movie find(Long id) {
         return entityManager.find(Movie.class, id);
     }
 
-    @Transactional
+    @Transactional(transactionManager = "moviesTransactionManager")
     public void addMovie(Movie movie) {
         entityManager.persist(movie);
     }
@@ -51,7 +51,7 @@ public class MoviesBean {
         entityManager.remove(movie);
     }
 
-    @Transactional
+    @Transactional(transactionManager = "moviesTransactionManager")
     public void deleteMovieId(long id) {
         Movie movie = entityManager.find(Movie.class, id);
         deleteMovie(movie);
